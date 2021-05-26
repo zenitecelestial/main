ea9ddfc3e244795058f8bf4df8326a012b6c225f
zenitecelestial <zenitecelestial@gmail.com>
2021-05-26 13:10:49 -0300

    adicionado FeignClient

--- consultaCepBR/src/main/java/br/com/consultaCep/consultaCepBR/ConsultaCepBrApplication.java
+++ consultaCepBR/src/main/java/br/com/consultaCep/consultaCepBR/ConsultaCepBrApplication.java
@@ -2,10 +2,11 @@ package br.com.consultaCep.consultaCepBR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@FeignClient
 public class ConsultaCepBrApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConsultaCepBrApplication.class, args);
 	}
